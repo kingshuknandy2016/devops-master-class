d803e378eb18e552a73748990f90e72e2e58dab0
Prathamesh Sarang <prathamesh.sarang@afourtech.com>
2020-05-13 15:37:20 +0530

    Added Unit test to test logic - whether expected values are retrieved for currency conversion

--- projects/microservices/01-currency-exchange-microservice-basic/src/test/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchangeServiceApplicationTests.java
+++ projects/microservices/01-currency-exchange-microservice-basic/src/test/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchangeServiceApplicationTests.java
@@ -1,7 +1,11 @@
 package com.in28minutes.microservices.currencyexchangeservice;
 
+import static org.junit.Assert.assertNotNull;
+
+import com.in28minutes.microservices.currencyexchangeservice.resource.CurrencyExchangeController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +13,12 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class CurrencyExchangeServiceApplicationTests {
 
+	@Autowired
+	private CurrencyExchangeController currencyExchangeController;
+
 	@Test
-	public void contextLoads() {
+	public void testApplicationContextLoads() {
+		assertNotNull(currencyExchangeController);
 	}
 
 }
